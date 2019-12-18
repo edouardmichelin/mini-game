04e31b56cab63dae209ab8eff0ab3dbd22a4e2bd
NervzZ <julien.jordan@epfl.ch>
2019-12-18 20:23:59 +0100

    fixed bomb and explosion anchor

--- java/ch/epfl/cs107/play/game/arpg/actor/Bomb.java
+++ java/ch/epfl/cs107/play/game/arpg/actor/Bomb.java
@@ -10,6 +10,7 @@ import ch.epfl.cs107.play.game.rpg.actor.RPGSprite;
 import ch.epfl.cs107.play.game.rpg.misc.DamageType;
 import ch.epfl.cs107.play.math.DiscreteCoordinates;
 import ch.epfl.cs107.play.math.RegionOfInterest;
+import ch.epfl.cs107.play.math.Vector;
 import ch.epfl.cs107.play.window.Canvas;
 
 import java.util.Collections;
@@ -40,9 +41,21 @@ public class Bomb extends AreaEntity implements Interactor, Dropable {
     }
 
     private Sprite[] getDefaultSprites() {
-        return new Sprite[] {
-                new RPGSprite(SpriteNames.BOMB, 1, 1, this, new RegionOfInterest(0, 0, 16, 16)),
-                new RPGSprite(SpriteNames.BOMB, 1, 1, this, new RegionOfInterest(16, 0, 16, 16))
+        return new Sprite[]{
+                new RPGSprite(
+                        SpriteNames.BOMB,
+                        1,
+                        1,
+                        this,
+                        new RegionOfInterest(0, 0, 16, 16),
+                        new Vector(0, 0.3f)),
+                new RPGSprite(
+                        SpriteNames.BOMB,
+                        1,
+                        1,
+                        this,
+                        new RegionOfInterest(16, 0, 16, 16),
+                        new Vector(0, 0.3f))
         };
     }
 
@@ -54,7 +67,8 @@ public class Bomb extends AreaEntity implements Interactor, Dropable {
                     1,
                     1,
                     this,
-                    new RegionOfInterest(frame * 32, 0, 32, 32)
+                    new RegionOfInterest(frame * 32, 0, 32, 32),
+                    new Vector(0, 0.2f)
             );
 
         return s;
@@ -118,7 +132,7 @@ public class Bomb extends AreaEntity implements Interactor, Dropable {
 
     @Override
     public boolean takeCellSpace() {
-        return false;
+        return true;
     }
 
     @Override
